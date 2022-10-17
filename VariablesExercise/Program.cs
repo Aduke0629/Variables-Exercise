f0c313fd035802e4c64f0f7993e699637a1ab2b8
Aduke0629 <aaron.duke0817@gmail.com>
2022-10-17 14:12:49 -0400

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            // Variable Declaration 
+            String name;
+            int age;
+            char mycharacter;
+            bool isdog;
+            double y = 4.4;
+            decimal x = 5;
+
+            // Variable Initialization
+
+            name = "kylo";
+            age = 3;
+            mycharacter = 'k';
+            isdog = true;
+            y = 4.5;
+            x = 5;
+
+
+
+           
+
+            Console.WriteLine($"My dog's name is {name}, He is {age} years old.");
         }
     }
 }
